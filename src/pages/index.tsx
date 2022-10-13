dfb9c7d70cac7082b8450a01adab5b56481269e3
hl11jp <nguyenducthien9@gmail.com>
2022-10-13 19:49:36 +0700

    added load more button and textSnippet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,45 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../graphql/generated/index";
 import { Layout } from "../components/Layout";
 import NextLink from "next/link";
-import { Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
 
 const Index = () => {
-  const [{data}] = usePostsQuery({variables: {limit: 10}});
+  const [{ data, fetching }] = usePostsQuery({ variables: { limit: 10 } });
+
+  if (!fetching && !data) {
+    return <div>Something went wrong</div>
+  }
+
   return (
     <Layout>
-      <NextLink href='/create-post'>
-        <Link>Create Post</Link>
-      </NextLink>
+      <Flex align="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">Create Post</Link>
+        </NextLink>
+      </Flex>
+
       <br></br>
-      {!data ? <div>loading...</div> : data.posts.map((post) => <div key={post.id}>{post.title}</div>)}
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        <Stack>
+          {data.posts.map((post) => (
+            <Box key={post.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{post.title}</Heading>
+              <Text mt={4}>{post.textSnippet}</Text>
+            </Box>
+            // <div key={post.id}>{post.title}</div>
+          ))}
+        </Stack>
+      )}
+      {data ? <Flex>
+        <Button isLoading={fetching} m="auto" my={8}>
+          load more
+        </Button>
+      </Flex> : null}
     </Layout>
   );
 };
 
-export default withUrqlClient(createUrqlClient, {ssr: true})(Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
